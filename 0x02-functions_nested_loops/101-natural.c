15d7c68eb5b7a54947efb2d2ce499c08e6f68d46
ermi1999 <ermiyasabiye1999@gmail.com>
2023-07-18 08:52:25 -0700

    fixed 6

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * this function that computes and prints the sum of all
+ * main - this function that computes and prints the sum of all
  * the multiples of 3 or 5
  * below 1024 (excluded), followed by a new line.
+ * Return: 0
  */
 int main(void)
 {
