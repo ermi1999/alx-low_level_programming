8554db2c14f701799aaddad2c32956cdbaecf8c2
ermi1999 <ermiyasabiye1999@gmail.com>
2023-07-19 01:27:51 -0700

    first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,10 +5,11 @@
 */
 int main(void)
 {
-int i;
+	int i;
 
-printf("Infinite loop incoming :(\n");
-i = 0;
+	printf("Infinite loop incoming :(\n");
+	
+	i = 0;
 
 /**
  *while (i < 10)
@@ -16,7 +17,8 @@ i = 0;
  *	putchar(i);
  *}
  */
-printf("Infinite loop avoided! \\o/\n");
-return (0);
+	printf("Infinite loop avoided! \\o/\n");
+	
+	return (0);
 }
 
