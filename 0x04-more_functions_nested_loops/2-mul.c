4d39e9b7f0501c813a454279285e7e05a8552261
ermi1999 <ermiyasabiye1999@gmail.com>
2023-07-20 04:52:06 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - will multiply two int
+ * a : the first intiger to be multiplied
+ * b : the second intiger to be multiplied
+ * return : multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
