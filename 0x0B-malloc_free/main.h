b8223f2f4a37581c3a795ed9581f37d942c1a243
ermi1999 <ermiyasabiye1999@gmail.com>
2023-08-07 02:12:00 -0700

    final

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+char *argstostr(int ac, char **av);
+#endif
