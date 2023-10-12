12bbb0a8893faa5de1c26fd424a032f6f5fdbe4f
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-12 13:46:25 -0700

    task 7

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,15 +7,13 @@
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
+
 	if (head == NULL)
 		return (0);
-	else
+	while (head != NULL)
 	{
-		while (head != NULL)
-		{
-			sum += head->n;
-			head = head->next;
-		}
+		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
