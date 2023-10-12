560c893b7a0af3a689c1fb278df4a21e03df0208
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-12 13:43:14 -0700

    task 7

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_dlistint - calculates the sum of all doubly linked list
+ * @head - pointer to the head of the node
+ * Return: the sum of the linked list if the list if empty 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	if (head == NULL)
+		return (0);
+	else
+	{
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
