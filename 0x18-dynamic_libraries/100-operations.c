45ab684e3f967ba39d28c279985e18ef382dc4a7
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-16 02:08:39 -0700

    task 3

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - adds two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - divides twonumbers and processes the reminder
+ * @a: the first number
+ * @b: the second number
+ * Return: the result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
