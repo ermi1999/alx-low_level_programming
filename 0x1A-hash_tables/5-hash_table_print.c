f820b5588a3f371eec9f72ffaa03b892c57c7876
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-03 02:47:17 -0700

    task 6

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: The hashtable.
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *entry;
+	int comma = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		entry = ht->array[i];
+
+		if (entry == NULL)
+			continue;
+		while (entry != NULL)
+		{
+			if (comma)
+				printf(", ");
+			printf("'%s': '%s'", entry->key, entry->value);
+
+			comma = 1;
+
+			entry = entry->next;
+		}
+	}
+	printf("}\n");
+}
