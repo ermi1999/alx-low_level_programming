26425bca6ca2e915e57ef388cd2bf55e7ccc7cb3
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-03 00:52:15 -0700

    task 5

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - gets the table assosiated with the key.
+ * @ht: the hash table.
+ * @key: The key.
+ * Return: The value.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *entry;
+
+	index = key_index((unsigned char *) key, ht->size);
+	entry = ht->array[index];
+
+	if (entry == NULL)
+		return NULL;
+	return (entry->value);
+}
