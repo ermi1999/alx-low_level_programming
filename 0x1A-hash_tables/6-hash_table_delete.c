05203feb1f0fba1c21ce15500cb000da6881244e
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-03 06:21:54 -0700

    fixed betty style

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,27 +6,27 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-        unsigned long int i;
-        hash_node_t *entry, *next;
+	unsigned long int i;
+	hash_node_t *entry, *next;
 
-        if (ht == NULL)
-                return;
-        for (i = 0; i < ht->size; i++)
-        {
-                if (ht->array[i] != NULL)
-                {
-                        entry = ht->array[i];
-                        while (entry != NULL)
-                        {
-                                next = entry->next;
-                                free(entry->key);
-                                free(entry->value);
-                                free(entry);
-                                entry = next;
-                        }
-                }
-                ht->array[i] = NULL;
-        }
-        free(ht->array);
-        free(ht);
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			entry = ht->array[i];
+			while (entry != NULL)
+			{
+				next = entry->next;
+				free(entry->key);
+				free(entry->value);
+				free(entry);
+				entry = next;
+			}
+		}
+		ht->array[i] = NULL;
+	}
+	free(ht->array);
+	free(ht);
 }
