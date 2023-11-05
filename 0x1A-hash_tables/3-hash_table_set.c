6d22f44698efadb6f46e51262bbba9106a23311c
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-05 05:29:10 -0800

    fixed updating a bucket

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(entry->value);
 			entry->value = strdup(value);
-			return(1);
+			return (1);
 		}
 		entry = entry->next;
 	}
