7731cc40e5a958e28998d3b5a56ad4380c87e22d
ermi1999 <ermiyasabiye1999@gmail.com>
2023-12-20 06:02:30 -0800

    island primeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,14 +8,17 @@ def island_perimeter(grid):
     """
     returns the perimeter of the island described in grid.
     """
-    result = 1
-    for i, ver in enumerate(grid):
-        for j, hor in enumerate(ver):
-            if ver[j] == 1:
-                if j > 0:
-                    if ver[j - 1] and ver[j + 1] == 1:
-                        result *= i * j
-                if i > 0:
-                    if grid[i - 1][j] and grid[i + 1][j] == 1:
-                        result *= i * j
+    result = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if j == 0 or grid[i][j - 1] == 0:
+                    result += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    result += 1
+                if i == 0 or grid[i - 1][j] == 0:
+                    result += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    result += 1
     return result
