d8186d7c46e1dba37bcb515753d985280c7b7a83
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-06 10:25:50 +0300

    fixed spelling error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,7 +18,7 @@ int binary_search(int *array, size_t size, int value)
 		size_t middle = left + (right - left) / 2;
 		size_t i;
 
-		printf("searching in array: ");
+		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
 			if (i == right)
