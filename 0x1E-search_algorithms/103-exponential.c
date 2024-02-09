51fa16b9206f332e94e14666cd04b020e56a365e
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-09 18:08:09 +0000

    exponential

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * binarySearch - search for a value using a binary searching algorithm.
+ * @array: the array in which the value to search for.
+ * @value: the value to search for.
+ * @start: the begining of the search.
+ * @end: the end of the search.
+ * Return: the index.
+ */
 int binarySearch(int *array, int value, size_t start, size_t end)
 {
 	if (array == NULL)
@@ -26,15 +34,32 @@ int binarySearch(int *array, int value, size_t start, size_t end)
 	}
 	return (-1);
 }
+
+/**
+ * exponential_search - searchs for a value using exponential searching algorithm.
+ * @array: the array in which the value to search for.
+ * @size: the size of the array.
+ * @value: the value to search for.
+ * Return: the index of the value.
+ */
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t bound = 1;
+	size_t start, end;
 
 	if (array == NULL)
 		return (-1);
 
 	while (bound < size && array[bound] < value)
-		bound *= bound;
+	{
+		printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
 
-	return binarySearch(array, value, bound / 2, (bound + 1 > size ? size : bound + 1));
+	start = bound / 2;
+	end = (bound < size) ? bound : size - 1;
+
+	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+
+	return binarySearch(array, value, start, end);
 }
